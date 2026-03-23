e7cf953e91dcb6452bc68f8dbf2d55699c481ef5
Aryan Parakh <Aryan.24BCS10336@sst.scaler.com>
2026-03-23 18:36:45 +0530

    Implement NormalDice class to roll a dice

---
+++ snake-and-ladder/NormalDice.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class NormalDice implements Dice {
+
+    private Random random = new Random();
+
+    @Override
+    public int roll() {
+        return random.nextInt(6) + 1;
+    }
+}
