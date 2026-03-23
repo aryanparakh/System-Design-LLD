c5f2b81671b46ef330e022d0f1a767b1b99e6a6b
Aryan Parakh <Aryan.24BCS10336@sst.scaler.com>
2026-03-23 16:05:31 +0530

    Add Vehicle class with properties and constructor

---
+++ Parking-Lot Design/Vehicle.java
@@ -0,0 +1,15 @@
+public class Vehicle {
+    private String vehicleNumber;
+    private VehicleType type;
+
+    public Vehicle(String vehicleNumber, VehicleType type) {
+        this.vehicleNumber = vehicleNumber;
+        this.type = type;
+    }
+    public VehicleType getType() {
+        return type;
+    }
+    public String getVehicleNumber() {
+        return vehicleNumber;
+    }
+}
