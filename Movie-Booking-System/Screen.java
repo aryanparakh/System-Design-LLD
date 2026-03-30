94a3e5c1a769653b87d2b326806c2f11bcacf97c
Aryan Parakh <Aryan.24BCS10336@sst.scaler.com>
2026-03-30 12:16:19 +0530

    Add Screen class for movie booking system

---
+++ Movie-Booking-System/Screen.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class Screen {
+    int id;
+    List<Seat> seats;
+
+    public Screen(int id, List<Seat> seats) {
+        this.id = id;
+        this.seats = seats;
+    }
+}
