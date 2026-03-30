4affa49d58eb4e6c0a8b69eab260b3bbb59e2478
Aryan Parakh <Aryan.24BCS10336@sst.scaler.com>
2026-03-30 12:05:23 +0530

    Add Request class for elevator system

---
+++ Elevator-System/Request.java
@@ -0,0 +1,9 @@
+public class Request {
+    int floor;
+    Direction direction;
+
+    public Request(int floor, Direction direction) {
+        this.floor = floor;
+        this.direction = direction;
+    }
+}
